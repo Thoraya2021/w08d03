7f32c877fcf19b2df86e33c1b52f3f1961347d9b
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-30 14:16:52 +0300

    main steps for authentication users

--- db/models/todo.js
+++ db/models/todo.js
@@ -4,5 +4,7 @@ const todoModel = new mongoose.Schema({
   name: { type: String, required: true },
   isDelelte: { type: Boolean, default: false },
   createby:  [{ type: mongoose.Schema.Types.ObjectId, ref:"user" }],
+  
 });
+
 module.exports =mongoose.model('Todo',todoModel)
